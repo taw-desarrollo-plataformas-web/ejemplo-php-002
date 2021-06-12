1b2baedc1ba042ea4600149e80b358c7f68228fc
@reroes <reroes799@gmail.com>
2021-06-11 21:09:18 -0500

    mofificando basedatos.php

--- ejemplo-php-002/basedatos.php
+++ ejemplo-php-002/basedatos.php
@@ -1,8 +1,8 @@
 <?php
   // datos para enlace la base de datos
   $servidor = "localhost";
-  $usuario = "root";
-  $password = "11112222";
+  $usuario = "su-usuario";
+  $password = "su-clave";
   $base_datos = "mobiliaria";
   $conectaBD = new mysqli($servidor, $usuario, $password, $base_datos);
   $conectaBD->set_charset("utf8");
